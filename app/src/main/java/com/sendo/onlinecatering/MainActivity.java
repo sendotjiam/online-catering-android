e098181d9c114f1c2e382ed457bfeb7591b54dc0
Hendry <hendryg56@gmail.com>
2021-05-12 16:24:32 +0700

    UI bagian hendry (check out, invoice, sama profile page)

--- app/src/main/java/com/sendo/onlinecatering/MainActivity.java
+++ app/src/main/java/com/sendo/onlinecatering/MainActivity.java
@@ -2,13 +2,29 @@ package com.sendo.onlinecatering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button sementara;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        sementara = findViewById(R.id.btn_sementara);
+
+
+    }
+
+    public void pindah(View view) {
+        Intent intent = new Intent(this, InvoicePage.class);
+        startActivity(intent);
     }
 }
