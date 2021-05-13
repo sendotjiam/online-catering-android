a35b6502eb78d7e4f2e358e3b3933eec4c1ebcfb
owenltm <owenlwiantoro01@gmail.com>
2021-05-13 20:17:00 +0800

    add all menu page

---
+++ app/src/main/java/com/sendo/onlinecatering/AllMenuPage.java
@@ -0,0 +1,14 @@
+package com.sendo.onlinecatering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllMenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_menu_page);
+    }
+}
