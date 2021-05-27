9ac3f37e47a6915d6e086ff89f814ab49f850fb8
Hendry <hendryg56@gmail.com>
2021-05-27 20:55:55 +0700

    hapus coba

--- app/src/main/java/com/sendo/onlinecatering/CheckOutPage.java
+++ app/src/main/java/com/sendo/onlinecatering/CheckOutPage.java
@@ -5,17 +5,33 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class CheckOutPage extends AppCompatActivity {
+    TextView olshopcash;
+    TextView totalpayment;
+    Button pay;
     RecyclerView item_view;
     ArrayList<CheckOutDetail> checkOutDetails = new ArrayList<>();
+    UsersDB usersDB;
+    Users users;
+    NumberFormat formatter = new DecimalFormat("#,###");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out_page);
+        olshopcash = findViewById(R.id.co_olcash);
+        totalpayment = findViewById(R.id.co_totalpayment);
+        pay = findViewById(R.id.btn_pay);
+
+        usersDB = new UsersDB(this);
 
         item_view = findViewById(R.id.rv_checkout);
 
@@ -48,5 +64,14 @@ public class CheckOutPage extends AppCompatActivity {
 
         item_view.setAdapter(checkOutDetailAdapter);
         item_view.setLayoutManager(new LinearLayoutManager(this));
+
+        //ingat ganti 1 nya jadi user_id juga
+        users = usersDB.getUser(1);
+
+        String formattampung = formatter.format(users.getWallet());
+        olshopcash.setText("Rp." + formattampung + ",00");
+    }
+
+    public void pay(View view) {
     }
 }
