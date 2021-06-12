01480e44b1f8082f876b0794511274acfef0115e
owenltm <owenlwiantoro01@gmail.com>
2021-06-12 10:58:14 +0800

    remove allMenu bottomNav

--- app/src/main/java/com/sendo/onlinecatering/AllMenuPage.java
+++ app/src/main/java/com/sendo/onlinecatering/AllMenuPage.java
@@ -38,8 +38,6 @@ public class AllMenuPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_menu_page);
 
-        navigation_bar();
-
         btnAddMenu = findViewById(R.id.btn_add_menu);
         rlMenuList = findViewById(R.id.rv_menu_list);
         btn_order = findViewById(R.id.btn_order);
@@ -73,41 +71,6 @@ public class AllMenuPage extends AppCompatActivity {
         super.onResume();
 
         menuAdapter.setMenuArrayList(menusDB.getMenus());
-    }
-
-    void navigation_bar() {
-        BottomNavigationView nav_admin = findViewById(R.id.nav_admin);
-
-        nav_admin.setSelectedItemId(R.id.menu_menus);
-        nav_admin.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
-                if (item.getItemId() == R.id.menu_menus) {
-                    return true;
-                }   else if (item.getItemId() == R.id.menu_order) {
-                    Toast.makeText(AllMenuPage.this, "orders", Toast.LENGTH_SHORT).show();
-
-                    Intent orderIntent = new Intent(AllMenuPage.this, CustomerOrder.class);
-                    orderIntent.putExtra("userid", userid);
-                    startActivity(orderIntent);
-
-                    return true;
-                } else if (item.getItemId() == R.id.menu_profile) {
-                    Toast.makeText(AllMenuPage.this, "profile", Toast.LENGTH_SHORT).show();
-
-                    //PROFILE
-                    /*Intent profileIntent = new Intent(AllMenuPage.this, CustomerOrder.class);
-                    profileIntent.putExtra("userid", userid);
-                    startActivity(profileIntent);*/
-
-                    return true;
-                } else {
-
-
-                    return true;
-                }
-            }
-        });
     }
 
     void orderIntent() {
